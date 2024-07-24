85fe3f27016a49c207f1c550d9d4dfc233e0d58f
Rosa Becerra <78577878+rosabecerra@users.noreply.github.com>
2024-07-24 12:00:32 +0200

    Update 02_📜_About.py

--- pages/02_📜_About.py
+++ pages/02_📜_About.py
@@ -6,23 +6,21 @@ st.markdown("# About")
 ##############################################################################
 
 st.markdown("""
-    We present the largest optical photometry compilation of Gamma-Ray Bursts (GRBs) with measured redshifts ($z$) ([Dainotti et al. 2024](https://doi.org/10.1093/mnras/stae1484)). 
-    Our dataset includes 64813 observations of 535 events (including upper limits) from 28 February 1997 to 18 August 2023. 
-    Moreover, we introduce **GRBLC**, a user-friendly web tool for visualising the photometry data, coordinates, redshift, host-galaxy extinction, 
-    and spectral indices for each event in our database. 
+
+    We present the largest optical photometry compilation of Gamma-Ray Bursts (GRBs) with measured redshifts ($z$) as detailed in ([Dainotti et al. 2024](https://doi.org/10.1093/mnras/stae1484)). 
+    Our dataset includes 64,813 observations of 535 events (including upper limits) from 28 February 1997 to 18 August 2023.
+    Moreover, we introduce GRBLC, a user-friendly web tool for visualizing the photometry data, coordinates, redshift, host-galaxy extinction, and spectral indices for each
+    event in our database.
+
+    Additionally, we have integrated a Gamma-ray Coordinate Network (GCN) scraper within **GRBLC** to automate the collection of magnitudes from GCN circulars. 
+    The web tool also includes a **Python** package for uniformly investigating color evolution in GRBs. We compute the optical spectral indices of 138 GRBs
+    and craft a novel procedure to infer the presence of color evolution in GRBs. By providing a standardized format and a centralized repository for optical
+    photometry, our web-based archive represents a significant milestone towards unifying various community efforts to collect GRB photometric data.
     
-    Additionally, we have integrated a Gamma-ray Coordinate Network (GCN) scraper 
-    within **GRBLC** to automate the collection of magnitudes from GCN circulars. The web tool also includes a **Python** package 
-    for uniformly investigating colour evolution in GRBs. We compute the optical spectral indices of 138 GRBs, 
-    and craft a novel procedure to infer the presence of colour evolution in GRBs. By providing a standardised format 
-    and a centralised repository for optical photometry, our web-based archive represents a significant milestone towards 
-    unifying various community efforts to collect GRB photometric data. 
-    
-    This comprehensive catalogue facilitates 
-    population studies by offering light curves (LCs) with improved coverage, as it aggregates data from multiple ground-based observatories 
-    and the Swift satellite. Consequently, these LCs can be employed to train future LC reconstructions, enhancing redshift inference capabilities. 
-    Our data collection efforts also help fill orbital gaps in the Swift observations, particularly at critical points in the LCs, 
-    such as the end of the plateau emission or the identification of jet breaks.
+    This comprehensive catalog facilitates population studies by offering light curves (LCs) with improved coverage, as it aggregates data from multiple ground-based
+    observatories and the Swift satellite. Consequently, these LCs can be employed to train future LC reconstructions, enhancing redshift inference capabilities. 
+    Our data collection efforts also help fill orbital gaps in the Swift observations, particularly at critical points in the LCs, such as the end of the plateau 
+    emission or the identification of jet breaks.
     """
 )
 
